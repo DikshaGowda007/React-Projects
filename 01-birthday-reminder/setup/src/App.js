b57f3d01edec786d24c39a3902f6ce34aec20f6f
Diksha Gowda <diksha.gowda@webenetic.com>
2023-08-22 16:30:12 +0530

    Complete

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,10 +1,25 @@
 import React, { useState } from 'react'
-import data from './data';
-import List from './List';
+import data from './data'
+import List from './List'
 
 const App = () => {
+  const [people, setPeople] = useState(data);
+  // console.log(`setPeople ${setPeople}`)
   return (
-    <div>App</div>
+    <>
+    {/* {JSON.stringify(data)} */}
+    {/* {JSON.stringify(people)} */}
+    <main>
+      <section className='container'>
+      <h3>{people.length} Birthdays Today</h3>
+      <List people={people} />
+      <button onClick={()=>{
+        console.log(`Clicked`);
+        setPeople([]);
+      }}>Clear All</button>
+      </section>
+    </main>
+    </>
   )
 }
 
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
-const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+const List = ({people}) => {
+  console.log(`People ${JSON.stringify(people.length)}`)
+  if (people.length !== undefined) {
+    return (
+      <>
+      {people.map((person)=>{
+        const { id, name, age, image } = person;
+        return <> <article key={id} className='person'>
+        <img src={image} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>{age} years</p>
+          <p className='remove_btn'>Remove</p>
+          {/* <button onClick={()=>{
+        console.log(`Clicked`);
+      }}>Clear All</button> */}
+        </div>
+        </article></>
+      })}
+      </>
+    );
+  } else {
+    return(
+      <>No Data Found ❗</>
+    )
+  }
 };
 
 export default List;
